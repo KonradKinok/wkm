44a99b381609576aa4eeecd69d0e2e8f23db76ee
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2024-10-01 18:49:15 +0200

    01.10.2024

--- src/pages/PenaltiesPage/PenaltiesPage.tsx
+++ src/pages/PenaltiesPage/PenaltiesPage.tsx
@@ -14,21 +14,61 @@ export default function PenaltiesPage() {
       <div className={scss["container-form"]}>
         <form className={scss["form"]} action="">
           <div className={scss["container-dateTimePicker"]}>
-            <label htmlFor="dateTimePicker">Wybierz datę:</label>
+            <label htmlFor="dateTimePicker">
+              Wybierz datę sporządzenia umowy, datę sprowadzenia pojazdu do
+              Polski:
+            </label>
             <DateTimePicker
               dateTimePickerDate={dateTimePickerDate}
               setDateTimePickerDate={setDateTimePickerDate}
             />
           </div>
-          <label htmlFor="toggle-switch">
-            <p className={scss["custom-title"]}>Free cancellation</p>
-          </label>
-          <input
-            type="checkbox"
-            name="toggle-switch"
-            id="toggle-switch"
-            className={scss["toggle-switch"]}
-          />
+          <div className={scss["container-radio"]}>
+            <label htmlFor="radio-sold">
+              <p className={scss["custom-title"]}>Sprzedałem pojazd:</p>
+            </label>
+            <input
+              type="radio"
+              name="amount_of_penalty"
+              id="radio-sold"
+              className={scss["toggle-switch"]}
+              defaultChecked={true}
+            />
+            <label htmlFor="radio-bought">
+              <p className={scss["custom-title"]}>
+                Kupiłem/sprowadziłem pojazd:
+              </p>
+            </label>
+            <input
+              type="radio"
+              name="amount_of_penalty"
+              id="radio-bought"
+              className={scss["toggle-switch"]}
+            />
+          </div>
+          <div className={scss["container-radio"]}>
+            <label htmlFor="natural-person">
+              <p className={scss["custom-title"]}>Jestem osobą fizyczną:</p>
+            </label>
+            <input
+              type="radio"
+              name="type_of_entity"
+              id="natural-person"
+              className={scss["toggle-switch"]}
+              defaultChecked={true}
+            />
+            <label htmlFor="legal-person">
+              <p className={scss["custom-title"]}>
+                Jestem przedsiębiorcą prowadzącym obrót pojazdami:
+              </p>
+            </label>
+            <input
+              type="radio"
+              name="type_of_entity"
+              id="legal-person"
+              className={scss["toggle-switch"]}
+            />
+          </div>
         </form>
         <p>
           {dateTimePickerDate ? dateTimePickerDate.toLocaleDateString() : ""}
