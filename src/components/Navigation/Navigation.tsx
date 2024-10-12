b6867b53145da6ae43fa378d9790f54128e03f47
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2024-10-13 00:51:45 +0200

    Update Navigation.tsx

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -5,26 +5,33 @@ import { selectLanguage } from "../redux/language/selectorsLanguage";
 import { langDictionary } from "../redux/language/constans";
 import scss from "./Navigation.module.scss";
 
-export const Navigation: React.FC = () => {
+interface NavigationProps {
+  onLinkClick?: () => void;
+}
+
+export const Navigation: React.FC<NavigationProps> = ({ onLinkClick }) => {
   const currentLanguage = useSelector(selectLanguage);
 
   return (
     <nav className={scss["nav-navigation"]}>
       <NavLink
         to="/"
-        className={({ isActive }) => (isActive ? scss.active : "")}>
+        className={({ isActive }) => (isActive ? scss.active : "")}
+        onClick={onLinkClick}>
         {langDictionary.navHome[currentLanguage]}
       </NavLink>
 
       <NavLink
         to="penalties"
-        className={({ isActive }) => (isActive ? scss.active : "")}>
+        className={({ isActive }) => (isActive ? scss.active : "")}
+        onClick={onLinkClick}>
         {langDictionary.navPenalties[currentLanguage]}
       </NavLink>
 
       <NavLink
         to="legalregulations"
-        className={({ isActive }) => (isActive ? scss.active : "")}>
+        className={({ isActive }) => (isActive ? scss.active : "")}
+        onClick={onLinkClick}>
         {langDictionary.navLegalRegulations[currentLanguage]}
       </NavLink>
     </nav>
